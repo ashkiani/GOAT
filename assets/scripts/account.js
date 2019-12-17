b05caad1a8d2d29a618fb362c97ff2d9c55ad0b9
Sia <ashkiani@yahoo.com>
2019-12-16 20:30:05 -0500

    Added code to populate the Shoe Size combo box

--- assets/scripts/account.js
+++ assets/scripts/account.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
     console.log("Document Ready!");
-    // var shoeSizeEl = $("#shoeSize");
-    // console.log(shoeSizeEl);
+    var shoeSizeEl = $("#shoeSize");
+    var defaultShoeSize = 10;
+    function clearShoeSizeCombo() {
+        shoeSizeEl.html("");
+    }
+
+    function addSize(size) {
+        var optEl = $("<option>");
+        optEl.html(size);
+        optEl.appendTo(shoeSizeEl);
+    }
+
+    function initShoeSizeCombo() {
+        clearShoeSizeCombo();
+        for (var i = 4; i < 13; i = i + 0.5) {
+            console.log(i);
+            addSize(i);
+        }
+        addSize(13);
+        addSize(14);
+        shoeSizeEl.val(defaultShoeSize);
+    }
+    initShoeSizeCombo();
+
 });
 
