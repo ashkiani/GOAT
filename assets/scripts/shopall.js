ec763199597710d2ba3af11d425737a4ed5850a0
nikola <nikola4work@gmail.com>
2019-12-19 06:34:07 -0500

    change the layout of img gallery, next step-filter the images

--- assets/scripts/shopall.js
+++ assets/scripts/shopall.js
@@ -28,38 +28,7 @@ function myFilter() {
 
 // ON SALE SLIDESHOW
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-    showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}
 
 // END OF ON SALE SLIDESHOW
 
@@ -77,4 +46,74 @@ $(document).ready(function() {
         $(this).addClass("active");
     });
     // END OF ON COMING SOON SLIDESHOW
+
+
+    // CELEBS GALLERY
+
+    $(".img-wrapper").hover(
+        function() {
+            $(this).find(".img-overlay").animate({ opacity: 1 }, 600);
+        },
+        function() {
+            $(this).find(".img-overlay").animate({ opacity: 0 }, 600);
+        }
+    );
+
+    var $overlay = $('<div id="overlay"></div>');
+    var $image = $("<img>");
+    var $prevButton = $('<div id="prevButton"><i class="fa fa-chevron-left"></i></div>');
+    var $nextButton = $('<div id="nextButton"><i class="fa fa-chevron-right"></i></div>');
+    var $exitButton = $('<div id="exitButton"><i class="fa fa-times"></i></div>');
+
+    $overlay.hide();
+
+    $(".img-overlay").click(function(event) {
+        event.preventDefault();
+        var imageLocation = $(this).prev().attr("href");
+        $image.attr("src", imageLocation);
+        $overlay.fadeIn("slow");
+    });
+
+    $overlay.click(function() {
+        $(this).fadeOut("slow");
+    });
+
+
+
+    var gallery = document.querySelector('#gallery');
+    var getVal = function(elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
+    var getHeight = function(item) { return item.querySelector('.content').getBoundingClientRect().height; };
+    var resizeAll = function() {
+        var altura = getVal(gallery, 'grid-auto-rows');
+        var gap = getVal(gallery, 'grid-row-gap');
+        gallery.querySelectorAll('.gallery-item').forEach(function(item) {
+            var el = item;
+            el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+        });
+    };
+    gallery.querySelectorAll('img').forEach(function(item) {
+        item.classList.add('byebye');
+        if (item.complete) {
+            console.log(item.src);
+        } else {
+            item.addEventListener('load', function() {
+                var altura = getVal(gallery, 'grid-auto-rows');
+                var gap = getVal(gallery, 'grid-row-gap');
+                var gitem = item.parentElement.parentElement;
+                gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+                item.classList.remove('byebye');
+            });
+        }
+    });
+    window.addEventListener('resize', resizeAll);
+    gallery.querySelectorAll('.gallery-item').forEach(function(item) {
+        item.addEventListener('click', function() {
+            item.classList.toggle('full');
+        });
+    });
+
+    // END OF CELEBS GALLERY
+
+
+
 });
