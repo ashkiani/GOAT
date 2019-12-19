a3c5c223153ad1c580c8549d238228d2c16329fd
Sia <ashkiani@yahoo.com>
2019-12-18 20:23:39 -0500

    Added code to load user data from the localStorage

--- assets/scripts/account.js
+++ assets/scripts/account.js
@@ -6,7 +6,76 @@ $(document).ready(function () {
         shoeSizeEl.html("");
     }
 
-    function underConstructionAlert(){
+    clearUserData();
+
+    function loadLocalStorage() {
+        var goatUsers = [userData];
+        var value = localStorage.getItem("goatUsers");
+        if (value !== null) {
+            console.log(value);
+            goatUsers = JSON.parse(value);
+        }
+        else {
+            console.log("No users in the local storage.");
+            goatUsers = null;
+        }
+        return goatUsers;
+    }
+
+    function getUserData(userName) {
+        userName = userName.trim();
+        if (userName != "") {
+            var goatUsers = [userData];
+            goatUsers = loadLocalStorage();
+            if (goatUsers !== null) {
+                goatUsers.forEach(user => {
+                    if (user !== null) {
+                        console.log("checking user:" & user.email);
+                        if (user.email == userName) {
+                            userData = user;
+                            return;
+                        }
+                    }
+                });
+
+            }
+        }
+    }
+
+    // function setUserData(user) {
+    //     var goatUsers = [userData];
+    //     goatUsers = loadLocalStorage();
+    //     if (goatUsers !== null) {
+
+
+    //     }
+    //     else {
+    //         goatUsers = [];
+
+    //     }
+    //     var userExists = false;
+    //     goatUsers.forEach(user => {
+    //         if (user !== null) {
+    //             console.log("checking user:" & user.email);
+    //             if (user.email == userName) {
+    //                 //copy the input into the exiting record
+    //                 userData.email=user.email;
+    //                 userData.name=user.name;
+    //                 userData.password=user.password;
+    //                 userData.shoeSize=user.shoeSize;
+
+    //                 return;
+    //             }
+    //         }
+    //     });
+    // }
+
+    // getUserData("ashkiani@yahoo.com");
+
+
+
+
+    function underConstructionAlert() {
         alert("This unit is under construction");
     }
     function addSize(size) {
@@ -28,10 +97,10 @@ $(document).ready(function () {
     initShoeSizeCombo();
 
 
-    $("#btnUpdate").click(function(){
+    $("#btnUpdate").click(function () {
         underConstructionAlert();
     });
-    $("#btnCancel").click(function(){
+    $("#btnCancel").click(function () {
         underConstructionAlert();
     });
 });
