ffbaa54d61128160e406738525b2f1e87d7dbc59
Marvin Castro <bbmarvin2003@yahoo.com>
2019-12-21 11:59:24 -0500

    update html and set up js file

---
+++ assets/scripts/cart.js
@@ -0,0 +1,22 @@
+//shoe description elements
+var priceEl = $('.price');
+var shoeNameEl = $('.shoe-name');
+var shoeSizeEl = $('.shoe-size');
+var shoeQuantityEl = $('.shoe-quantity');
+var shoePriceEl = $('.shoe-price');
+// shoe price elements 
+var subtotalEl = $('.price-subtotal');
+var priceItems = $('.price-items');
+// MAke Price discont a Button 
+var estimatedTotal = $('.estimated-total');
+
+if (userData.cart.length > 0 ){
+    for (var i=0; i < userData.cart.length; i++){
+    }
+}else{
+
+    alert('empty');
+}
+userData.email==""
+// if user email is empty no one is logged in if string is full some is 
+// is logged in 
