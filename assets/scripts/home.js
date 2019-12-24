56b3951ee1cdb47a93b877241ed5264e2891e839
Gene Edwards <eedwards@su.edu>
2019-12-24 09:06:57 -0500

    Added iTunes api to the home page

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -42,3 +42,104 @@ $("#feature-display").hover(function(){
   $("#img-div1").fadeIn(5000);
   
 })
+
+$("#btnSearch").click(function(){
+
+  $("#searchResults").empty();
+ 
+  var keywordSearched = $("#txtSearch").val();
+  var queryURL = `https://itunes.apple.com/search?term=${keywordSearched}&entity=album`;
+  
+   // Performing our AJAX GET request
+   $.ajax({
+    url: queryURL,
+    method: "GET",
+    dataType: "jsonp",
+    headers: {
+      'Access-Control-Allow-Credentials' : true,
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-Methods':'GET',
+      'Access-Control-Allow-Headers':'application/json',
+    }
+  })
+    // After the data comes back from the API
+    .then(function(response) {
+    
+      // Storing an array of results in the results variable
+      var results = response.results;
+
+      // Using for loop to create cards for each album returned
+      for (var i = 0; i < results.length; i++) {
+
+        var cardDiv = $("<div>");
+        cardDiv.addClass("card");
+        cardDiv.css("margin-bottom", "5px");
+       
+
+        var cardContentDiv = $("<div>");
+        cardContentDiv.addClass("card-content");
+
+        var mediaDiv = $("<div>");
+        mediaDiv.addClass("media");
+
+        var mediaLeftDiv = $("<div>");
+        mediaLeftDiv.addClass("media-left");
+
+        var figure = $("<figure>");
+        figure.addClass("image is-48x48");
+
+        var image = $("<img>");
+        image.attr("src", response.results[i].artworkUrl100);
+
+        var mediaContent = $("<div>");
+        mediaContent.addClass("media-content");
+
+        var pArtistName = $("<p>");
+        pArtistName.addClass("title is-4");
+        pArtistName.text(response.results[i].artistName);
+
+        var pAlbum = $("<p>");
+        pAlbum.addClass("subtitle is-6");
+        pAlbum.text(response.results[i].collectionName);
+
+        var contentDiv = $("<div>");
+        contentDiv.addClass("content");
+
+        var pDescription = $("<p>");
+        pDescription.text(`The ${response.results[i].collectionName} album contains ${response.results[i].trackCount} tracks. The genre has been categorized as ${response.results[i].primaryGenreName}.`);
+        
+        var pPrice = $("<p>");
+        pPrice.text(`You can purchase this album for $${response.results[i].collectionPrice}.`);
+
+        var buttonsDiv = $("<div>");
+        buttonsDiv.addClass("buttons");
+
+        var btnProfile = $("<a>");
+        btnProfile.text("Artist Profile");
+        btnProfile.addClass("button is-link");
+        btnProfile.attr({target: "blank_", href: response.results[i].artistViewUrl})
+
+        var btnPreview = $("<a>");
+        btnPreview.text("Preview Album");
+        btnPreview.addClass("button is-success");
+        btnPreview.attr({target: "blank_", href: response.results[i].collectionViewUrl});
+
+
+        $("#searchResults").append(cardDiv);
+        cardDiv.prepend(cardContentDiv);
+        cardContentDiv.append(mediaDiv, contentDiv);
+        mediaDiv.append(mediaLeftDiv, mediaContent);
+        mediaLeftDiv.append(figure);
+        figure.append(image);
+        mediaContent.append(pArtistName, pAlbum);
+        contentDiv.append(pDescription, pPrice, buttonsDiv);
+        buttonsDiv.append(btnProfile, btnPreview);
+   
+      }
+      console.log(results);
+   
+      //console.log(response);
+
+    });
+
+})
