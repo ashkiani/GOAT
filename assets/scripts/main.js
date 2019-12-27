26ce8233725b9efb1021dd2f096a2fb4eba9bb11
Sia <ashkiani@yahoo.com>
2019-12-27 13:02:29 -0500

    Updated the SignUp click function to validate the entered data before the save operation.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,143 +1,153 @@
 var partJson = {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
         "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
+        "value_area": 800
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
       }
     },
-    "retina_detect": true
-  };
-  var jsonUri = "data:text/plain;base64,"+window.btoa(JSON.stringify(partJson));
-  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('particles-js', jsonUri, function() {
-    console.log('callback - particles.js config loaded');
-  });
-  $(document).ready(function () {
-    
-    
-    $("#btnLogin").click(function () {
-      var userName = $("#username").val();
-      getUserData(userName);
-      if(userData.email ==""){
-        alert("loginFailed");
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
       }
-      else{
-        alert("welcome " + userName.email);
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
       }
-    });
-    
-    $("#btnSignUp").click(function () {
-     
-        var userName = $("#userName").val();
-        var password = $("#password").val();
-        // add validation here
-console.log(userName);
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+};
+var jsonUri = "data:text/plain;base64," + window.btoa(JSON.stringify(partJson));
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', jsonUri, function () {
+  console.log('callback - particles.js config loaded');
+});
+$(document).ready(function () {
 
-        userData.email=userName;
-        userData.password=password;
-        setUserData(userData);
-      });
-  
+
+  $("#btnLogin").click(function () {
+    var userName = $("#username").val();
+    getUserData(userName);
+    if (userData.email == "") {
+      alert("loginFailed");
+    }
+    else {
+      alert("welcome " + userName.email);
+    }
   });
-  
+
+  // Siavash 12/27/2019
+  // Updated the SignUp click function to validate the entered data before the save operation. 
+  $("#btnSignUp").click(function () {
+
+    var userDataObject = generateBlankUserDataObject();
+    userDataObject.email = $("#userName").val();
+    userDataObject.password = $("#password").val();
+    var errors = getUserDataObjectErrors(userDataObject);
+    if (errors.length > 0) {
+      // Siavash 12/27/2019
+      // Add your code here to show the content of errors to the user.
+      // The For loop below is an example of how to print the errors in the console. 
+      for (var i = 0; i < errors.length; i++) {
+        console.log("Error " + (i + 1) + ": " + errors[i]);
+      }
+    }
+    else {
+      setUserData(userDataObject);
+    }
+
+  });
+
+});
+
